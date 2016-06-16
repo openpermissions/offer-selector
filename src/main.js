59e44fe5c3a5b686bcc40e7c6b779b10a1870498
Andy Boscor <andyboscor@yahoo.com>
2016-06-16 10:30:06 +0100

    Work in progress

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const riot = require('riot');
+require('./templates/offers.tag');
+/*
+riot.mount('offers', {
+  title: 'OPP License Offers',
+  items: [
+    { type: "Commercial Website", color: "color1"},
+    { type: "Non-commercial Website", color: "color2"},
+    { type: "Advertisement", color: "color1"},
+    { type: "Non-commercial Website", color: "color2"},
+    { type: "Non-commercial Website", color: "color2"},
+    { type: "Advertisement", color: "color1"},
+    { type: "Advertisement", color: "color1"},
+  ]
+})
+*/
