6c9b8e243b97c5304bf229fd9df81fba48206bed
Tessa Conway <tessa.conway@cde.catapult.org.uk>
2016-06-28 16:46:07 +0100

    Update tests

--- test/offer.js
+++ test/offer.js
@@ -12,11 +12,8 @@ describe('parseOffer', () => {
     options = {
       organisations: 'https://acc.copyrighthub.org/v1/accounts/organisations',
       defaults: {
-        color: 'color1',
-        title_color: '#379392',
-        logo_color : '#353866',
-        btn_text_color: 'white',
-        primary_color: '#CE6D39'
+        primary_color: '#ffffff',
+        secondary_color: '#000000'
       }
     };
   });
@@ -28,15 +25,9 @@ describe('parseOffer', () => {
       offer_id: 'e3138acd145f484e9c5601685d5166f8',
       type : 'Non-commercial Website',
       description: 'Use an image on a blog or website<br/><br/>Site does not carry advertising or sell products or services.<br/>Site receives no more than 50,000 views per month<br/>Maximum size of image 400 x 400px.',
-      color: 'color1',
-      title_color: '#379392',
-      logo_color: '#353866',
-      btn_text_color: 'white',
-      primary_color: '#CE6D39',
-      organisation: {
-        id: 'orgid1',
-        name: 'Organisation 1'
-      },
+      logo: undefined,
+      primary_color: '#ffffff',
+      secondary_color: '#000000',
       price: {
         value: '1',
         unit: 'GBP'
@@ -127,11 +118,8 @@ describe('parseOffer', () => {
     let expected = {
       id: 'e3138acd145f484e9c5601685d5166f8',
       offer_id: 'e3138acd145f484e9c5601685d5166f8',
-      color: 'color1',
-      title_color: '#379392',
-      logo_color: '#353866',
-      btn_text_color: 'white',
-      primary_color: '#CE6D39',
+      primary_color: '#ffffff',
+      secondary_color: '#000000',
       price: {
         value: '1',
         unit: 'GBP'
@@ -191,11 +179,8 @@ describe('parseOffer', () => {
     let expected = {
       id: 'e3138acd145f484e9c5601685d5166f8',
       offer_id: 'e3138acd145f484e9c5601685d5166f8',
-      color: 'color1',
-      title_color: '#379392',
-      logo_color: '#353866',
-      btn_text_color: 'white',
-      primary_color: '#CE6D39',
+      primary_color: '#ffffff',
+      secondary_color: '#000000',
       price: {
         value: '1',
         unit: '😱 '
@@ -220,11 +205,8 @@ describe('parseOffer', () => {
     let expected = {
       id: 'e3138acd145f484e9c5601685d5166f8',
       offer_id: 'e3138acd145f484e9c5601685d5166f8',
-      color: 'color1',
-      title_color: '#379392',
-      logo_color: '#353866',
-      btn_text_color: 'white',
-      primary_color: '#CE6D39'
+      primary_color: '#ffffff',
+      secondary_color: '#000000',
     };
 
     return offer.parseOffer(data, options)
@@ -303,11 +285,8 @@ describe('parseOffer', () => {
     let expected = {
       id: 'e3138acd145f484e9c5601685d5166f8',
       offer_id: 'e3138acd145f484e9c5601685d5166f8',
-      color: 'color1',
-      title_color: '#379392',
-      logo_color: '#353866',
-      btn_text_color: 'white',
-      primary_color: '#CE6D39',
+      primary_color: '#ffffff',
+      secondary_color: '#000000',
       price: {
         value: '1',
         unit: 'GBP'
@@ -396,11 +375,8 @@ describe('parseOffer', () => {
     let expected = {
       id: 'e3138acd145f484e9c5601685d5166f8',
       offer_id: 'e3138acd145f484e9c5601685d5166f8',
-      color: 'color1',
-      title_color: '#379392',
-      logo_color: '#353866',
-      btn_text_color: 'white',
-      primary_color: '#CE6D39',
+      primary_color: '#ffffff',
+      secondary_color: '#000000',
       price: {
         value: '1',
         unit: 'GBP'
